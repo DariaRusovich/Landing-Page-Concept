8095d9f42e9d207d559e4833d1372e0226e434fe
DariaRusovich <rusovichdasha41@gmail.com>
2021-07-21 15:36:10 +0300

    add-event-to-close-btn

--- js/app.js
+++ js/app.js
@@ -1,31 +1,30 @@
+const modalWindow = document.getElementById("modalWindow");
+const modalWindowWrapper = document.getElementById("modalWindowWrapper");
+const closeBtn = document.getElementById("closeBtn");
 
-const modalWindow = document.getElementById('modalWindow')
-const modalWindowWrapper = document.getElementById('modalWindowWrapper')
-
-modalWindowWrapper.addEventListener('click', event => {
-    // const playBtn = document.getElementById('playVideoBtn')
-    const modalBackdrop = document.getElementById('modalBackdrop')
-    const playBtn = event.target.closest('.play-btn')
-    console.log(playBtn);
-    if (playBtn){
-        openModal();
-    }
-    else if(event.target === modalBackdrop){
-        closeModal();
-        console.log(modalBackdrop);
-    }
-})
-document.addEventListener('keyup', event => {
-    if (event.key === 'Escape'){
-        closeModal();
-    }
-})
+modalWindowWrapper.addEventListener("click", (event) => {
+  // const playBtn = document.getElementById('playVideoBtn')
+  const modalBackdrop = document.getElementById("modalBackdrop");
+  const playBtn = event.target.closest(".play-btn");
+  if (playBtn) {
+    openModal();
+  } else if (event.target === modalBackdrop) {
+    closeModal();
+  }
+});
+closeBtn.addEventListener("click", (e) => {
+  closeModal();
+});
+document.addEventListener("keyup", (event) => {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
 
 function openModal() {
-    document.body.classList.add('show-modal')
+  document.body.classList.add("show-modal");
 }
 
-
 function closeModal() {
-    document.body.classList.remove('show-modal')
+  document.body.classList.remove("show-modal");
 }
